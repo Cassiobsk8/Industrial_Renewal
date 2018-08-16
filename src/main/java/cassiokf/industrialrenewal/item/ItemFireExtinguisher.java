6b725e7bb559277fe6dfb5b9be8f550d438498db
CassioKF <cassiobsk8@hotmail.com>
2018-08-15 22:51:20 -0300

    Fire extinguisher now it cools lava

--- src/main/java/cassiokf/industrialrenewal/item/ItemFireExtinguisher.java
+++ src/main/java/cassiokf/industrialrenewal/item/ItemFireExtinguisher.java
@@ -2,12 +2,17 @@ package cassiokf.industrialrenewal.item;
 
 import cassiokf.industrialrenewal.blocks.BlockFireExtinguisher;
 import cassiokf.industrialrenewal.blocks.ModBlocks;
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Blocks;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.*;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import net.minecraftforge.fluids.BlockFluidBase;
 
 import java.util.ArrayList;
 import java.util.Objects;
@@ -25,12 +30,9 @@ public class ItemFireExtinguisher extends ItemBase {
     public EnumActionResult onItemUse(EntityPlayer player, World worldIn, BlockPos pos, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ) {
         ItemStack itemstack = player.getHeldItem(hand);
         BlockPos posOffset = pos.offset(facing);
-        int i = pos.getX();
-        int j = pos.getY();
-        int k = pos.getZ();
         if (player.isSneaking()) {
             if (worldIn.isAirBlock(posOffset) || worldIn.getBlockState(posOffset).getBlock().isReplaceable(worldIn, posOffset)) {
-                worldIn.playSound(null, (double) i + 0.5D, (double) j + 0.5D, (double) k + 0.5D, Objects.requireNonNull(SoundEvent.REGISTRY.getObject(new ResourceLocation(("block.metal.place")))), SoundCategory.BLOCKS, 1.0F, 1.0F);
+                playSound(worldIn, pos, "block.metal.place");
                 worldIn.setBlockState(posOffset, ModBlocks.fireExtinguisher.getDefaultState().withProperty(BlockFireExtinguisher.FACING, player.getHorizontalFacing()).withProperty(BlockFireExtinguisher.ONWALL, facing != EnumFacing.UP));
                 itemstack.shrink(1);
                 return EnumActionResult.SUCCESS;
@@ -42,14 +44,27 @@ public class ItemFireExtinguisher extends ItemBase {
             for (BlockPos bpos : list) {
                 worldIn.spawnParticle(EnumParticleTypes.WATER_SPLASH, bpos.offset(facing).getX() + 0.5D, bpos.offset(facing).getY() + 0.5D, bpos.offset(facing).getZ() + 0.5D, 0, 1, 0);
                 for (EnumFacing faces : EnumFacing.values()) {
+                    IBlockState state = worldIn.getBlockState(bpos);
+                    Block block = state.getBlock();
+                    if (worldIn.isRemote) System.out.println(block.getRegistryName());
                     if (worldIn.extinguishFire(player, bpos, faces)) {
                         worldIn.spawnParticle(EnumParticleTypes.SMOKE_NORMAL, bpos.getX() + 0.5D, bpos.getY() + 0.5D, bpos.getZ() + 0.5D, 0, 1, 0);
-                        worldIn.playSound(null, (double) i + 0.5D, (double) j + 0.5D, (double) k + 0.5D, Objects.requireNonNull(SoundEvent.REGISTRY.getObject(new ResourceLocation(("block.fire.extinguish")))), SoundCategory.BLOCKS, 1.0F, 1.0F);
+                        playSound(worldIn, pos, "block.fire.extinguish");
+                    }
+                    if (state.getMaterial() == Material.LAVA && state.getValue(BlockFluidBase.LEVEL) == 0) {
+                        worldIn.setBlockState(bpos, Blocks.OBSIDIAN.getDefaultState());
+                    }
+                    if (state.getMaterial() == Material.LAVA && state.getValue(BlockFluidBase.LEVEL) > 0) {
+                        worldIn.setBlockState(bpos, Blocks.COBBLESTONE.getDefaultState());
                     }
                 }
             }
             return EnumActionResult.SUCCESS;
         }
+    }
+
+    private void playSound(World world, BlockPos pos, String resourceLocation) {
+        world.playSound(null, pos, Objects.requireNonNull(SoundEvent.REGISTRY.getObject(new ResourceLocation((resourceLocation)))), SoundCategory.BLOCKS, 1.0F, 1.0F);
     }
 
     private void add9x9pos(ArrayList<BlockPos> list, BlockPos initialPos) {
