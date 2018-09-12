7e3444c7dc701d84d45299a6a063585d9edec795
CassioKF <cassiobsk8@hotmail.com>
2018-09-12 11:42:42 -0300

    Fix get world on packet receive

--- src/main/java/cassiokf/industrialrenewal/network/PacketCargoLoader.java
+++ src/main/java/cassiokf/industrialrenewal/network/PacketCargoLoader.java
@@ -4,6 +4,8 @@ import cassiokf.industrialrenewal.tileentity.railroad.cargoloader.TileEntityCarg
 import io.netty.buffer.ByteBuf;
 import net.minecraft.client.Minecraft;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.minecraftforge.fml.common.FMLCommonHandler;
 import net.minecraftforge.fml.common.network.simpleimpl.IMessage;
 import net.minecraftforge.fml.common.network.simpleimpl.IMessageHandler;
 import net.minecraftforge.fml.common.network.simpleimpl.MessageContext;
@@ -11,16 +13,18 @@ import net.minecraftforge.fml.common.network.simpleimpl.MessageContext;
 public class PacketCargoLoader implements IMessage {
 
     private BlockPos pos;
+    private int dimension;
     private int enumConfig;
 
 
-    public PacketCargoLoader(BlockPos pos, int enumConfig) {
+    public PacketCargoLoader(BlockPos pos, int dimension, int enumConfig) {
         this.pos = pos;
+        this.dimension = dimension;
         this.enumConfig = enumConfig;
     }
 
     public PacketCargoLoader(TileEntityCargoLoader te) {
-        this(te.getPos(), te.getWaitEnum().intValue);
+        this(te.getPos(), te.getWorld().provider.getDimension(), te.getWaitEnum().intValue);
     }
 
     public PacketCargoLoader() {
@@ -29,12 +33,14 @@ public class PacketCargoLoader implements IMessage {
     @Override
     public void toBytes(ByteBuf buf) {
         buf.writeLong(pos.toLong());
+        buf.writeInt(dimension);
         buf.writeInt(enumConfig);
     }
 
     @Override
     public void fromBytes(ByteBuf buf) {
         pos = BlockPos.fromLong(buf.readLong());
+        dimension = buf.readInt();
         enumConfig = buf.readInt();
     }
 
@@ -43,8 +49,11 @@ public class PacketCargoLoader implements IMessage {
         @Override
         public IMessage onMessage(PacketCargoLoader message, MessageContext ctx) {
             Minecraft.getMinecraft().addScheduledTask(() -> {
-                TileEntityCargoLoader te = (TileEntityCargoLoader) Minecraft.getMinecraft().world.getTileEntity(message.pos);
-                te.setWaitEnum(message.enumConfig);
+                World world = FMLCommonHandler.instance().getMinecraftServerInstance().getWorld(message.dimension);
+                TileEntityCargoLoader te = (TileEntityCargoLoader) world.getTileEntity(message.pos);
+                if (te != null) {
+                    te.setWaitEnum(message.enumConfig);
+                }
                 return null;//new PacketReturnCargoLoader(te);
             });
             return null;
