5140c16757b58ddf0f61a4d9104eb5bff9e8cec3
CassioKF <cassiobsk8@hotmail.com>
2019-06-07 02:20:08 -0300

    Forge 2816 and 0.10.0 preparation

--- src/main/java/cassiokf/industrialrenewal/References.java
+++ src/main/java/cassiokf/industrialrenewal/References.java
@@ -10,7 +10,7 @@ public class References {
 
     public static final String NAME = "Industrial Renewal";
 
-    public static final String VERSION = "0.9";
+    public static final String VERSION = "0.10.0";
 
     public static final String NETWORKCHANNEL = MODID;
 
