d395804cd4de2ad5def7bb1d7c9121924ed2d3e7
CassioKF <cassiobsk8@hotmail.com>
2019-06-28 23:02:47 -0300

    Roof can now be rotated

--- src/main/java/cassiokf/industrialrenewal/blocks/BlockRoof.java
+++ src/main/java/cassiokf/industrialrenewal/blocks/BlockRoof.java
@@ -1,16 +1,17 @@
 package cassiokf.industrialrenewal.blocks;
 
-import com.google.common.collect.ImmutableList;
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockHorizontal;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
-import net.minecraft.block.properties.IProperty;
 import net.minecraft.block.properties.PropertyBool;
+import net.minecraft.block.properties.PropertyDirection;
 import net.minecraft.block.state.BlockFaceShape;
 import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.util.EnumBlockRenderType;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.AxisAlignedBB;
@@ -20,13 +21,16 @@ import net.minecraft.world.World;
 
 import javax.annotation.Nullable;
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class BlockRoof extends BlockBase {
 
-    public static final ImmutableList<IProperty<Boolean>> CONNECTED_PROPERTIES = ImmutableList.copyOf(
-            Stream.of(EnumFacing.VALUES).map(facing -> PropertyBool.create(facing.getName())).collect(Collectors.toList()));
+    public static final PropertyDirection FACING = BlockHorizontal.FACING;
+    public static final PropertyBool SOUTH = PropertyBool.create("south");
+    public static final PropertyBool NORTH = PropertyBool.create("north");
+    public static final PropertyBool EAST = PropertyBool.create("east");
+    public static final PropertyBool WEST = PropertyBool.create("west");
+    public static final PropertyBool DOWN = PropertyBool.create("down");
+
     protected static final AxisAlignedBB FULL_AABB = new AxisAlignedBB(0.0D, 0.0D, 0.0D, 1.0D, 1.0D, 1.0D);
     protected static final AxisAlignedBB BASE_AABB = new AxisAlignedBB(0.0D, 0.75D, 0.0D, 1.0D, 1.0D, 1.0D);
     protected static final AxisAlignedBB BOT_AABB = new AxisAlignedBB(0.0D, 0.0D, 0.0D, 1.0D, 0.875D, 1.0D);
@@ -40,18 +44,20 @@ public class BlockRoof extends BlockBase {
 
     @Override
     protected BlockStateContainer createBlockState() {
-        return new BlockStateContainer(this, CONNECTED_PROPERTIES.toArray(new IProperty[CONNECTED_PROPERTIES.size()]));
+        return new BlockStateContainer(this, FACING, SOUTH, NORTH, EAST, WEST, DOWN);
     }
 
     @SuppressWarnings("deprecation")
     @Override
-    public IBlockState getStateFromMeta(final int meta) {
-        return getDefaultState();
+    public IBlockState getStateFromMeta(int meta)
+    {
+        return getDefaultState().withProperty(FACING, EnumFacing.getHorizontal(meta));
     }
 
     @Override
-    public int getMetaFromState(final IBlockState state) {
-        return 0;
+    public int getMetaFromState(IBlockState state)
+    {
+        return state.getValue(FACING).getIndex();
     }
 
     @SuppressWarnings("deprecation")
@@ -71,8 +77,10 @@ public class BlockRoof extends BlockBase {
         return EnumBlockRenderType.MODEL;
     }
 
-    private Boolean isEven(BlockPos pos) {
+    private Boolean isEven(BlockPos pos, boolean baseOnX)
+    {
         Integer number = pos.getZ();
+        if (baseOnX) number = pos.getX();
         if ((number % 2) == 0) {
             return true;
         } else {
@@ -80,39 +88,46 @@ public class BlockRoof extends BlockBase {
         }
     }
 
-    protected boolean isValidConnection(final IBlockAccess world, final BlockPos ownPos, final EnumFacing neighbourDirection) {
+    protected boolean canConnectTo(final IBlockAccess world, final BlockPos ownPos, EnumFacing ownFacing, final EnumFacing neighbourDirection)
+    {
         final BlockPos neighbourPos = ownPos.offset(neighbourDirection);
         final IBlockState neighbourState = world.getBlockState(neighbourPos);
         Block nb = neighbourState.getBlock();
-        if ((neighbourDirection == EnumFacing.EAST || neighbourDirection == EnumFacing.WEST)) {
+        if (((neighbourDirection == EnumFacing.EAST || neighbourDirection == EnumFacing.WEST) && (ownFacing == EnumFacing.SOUTH || ownFacing == EnumFacing.NORTH))
+                || ((neighbourDirection == EnumFacing.SOUTH || neighbourDirection == EnumFacing.NORTH) && (ownFacing == EnumFacing.WEST || ownFacing == EnumFacing.EAST)))
+        {
             IBlockState dState = world.getBlockState(ownPos.offset(EnumFacing.DOWN));
             IBlockState sState = world.getBlockState(ownPos.offset(EnumFacing.SOUTH));
             IBlockState nState = world.getBlockState(ownPos.offset(EnumFacing.NORTH));
-            Block dBlock = dState.getBlock();
             Block sBlock = sState.getBlock();
             Block nBlock = nState.getBlock();
-            if ((sBlock instanceof BlockRoof || sBlock instanceof BlockCatwalkLadder || sBlock.isFullCube(sState)) && (nBlock instanceof BlockRoof || nBlock instanceof BlockCatwalkLadder || nBlock.isFullCube(nState))) {
+            if ((sBlock instanceof BlockRoof || sBlock instanceof BlockCatwalkLadder || sState.isFullCube()) && (nBlock instanceof BlockRoof || nBlock instanceof BlockCatwalkLadder || nState.isFullCube()))
+            {
                 // (block pos is Even) && (neighbour SW) && !down connection
-                return isEven(ownPos)
-                        && (nb instanceof BlockRoof || nb.isFullCube(neighbourState) || nb instanceof BlockPillar || nb instanceof BlockColumn)
-                        && !dBlock.isFullCube(dState);
+                boolean invert = ownFacing == EnumFacing.WEST || ownFacing == EnumFacing.EAST;
+                return isEven(ownPos, invert)
+                        && (nb instanceof BlockRoof || neighbourState.isFullCube() || nb instanceof BlockPillar || nb instanceof BlockColumn)
+                        && !dState.isFullCube();
             }
         }
-        return neighbourDirection == EnumFacing.DOWN && nb.isFullCube(neighbourState);
+        return neighbourDirection == EnumFacing.DOWN && neighbourState.isFullCube();
     }
 
     @SuppressWarnings("deprecation")
     @Override
     public IBlockState getActualState(IBlockState state, final IBlockAccess world, final BlockPos pos) {
-        for (final EnumFacing facing : EnumFacing.VALUES) {
-            state = state.withProperty(CONNECTED_PROPERTIES.get(facing.getIndex()),
-                    isValidConnection(world, pos, facing));
-        }
+        EnumFacing facing = state.getValue(FACING);
+        state = state.withProperty(SOUTH, canConnectTo(world, pos, facing, facing.getOpposite())).withProperty(NORTH, canConnectTo(world, pos, facing, facing))
+                .withProperty(EAST, canConnectTo(world, pos, facing, facing.rotateY())).withProperty(WEST, canConnectTo(world, pos, facing, facing.rotateYCCW()))
+                .withProperty(DOWN, canConnectTo(world, pos, facing, EnumFacing.DOWN));
         return state;
     }
 
-    public final boolean isConnected(final IBlockState state, final EnumFacing facing) {
-        return state.getValue(CONNECTED_PROPERTIES.get(facing.getIndex()));
+    @SuppressWarnings("deprecation")
+    @Override
+    public IBlockState getStateForPlacement(World worldIn, BlockPos pos, EnumFacing facing, float hitX, float hitY, float hitZ, int meta, EntityLivingBase placer)
+    {
+        return getDefaultState().withProperty(FACING, placer.getHorizontalFacing());
     }
 
     @SuppressWarnings("deprecation")
@@ -120,7 +135,8 @@ public class BlockRoof extends BlockBase {
     public void addCollisionBoxToList(IBlockState state, final World worldIn, final BlockPos pos, final AxisAlignedBB entityBox, final List<AxisAlignedBB> collidingBoxes, @Nullable final Entity entityIn, final boolean isActualState) {
         IBlockState actualState = state.getActualState(worldIn, pos);
         addCollisionBoxToList(pos, entityBox, collidingBoxes, BASE_AABB);
-        if (isConnected(actualState, EnumFacing.DOWN) || isConnected(actualState, EnumFacing.EAST) || isConnected(actualState, EnumFacing.WEST)) {
+        if (actualState.getValue(DOWN) || actualState.getValue(EAST) || actualState.getValue(WEST))
+        {
             addCollisionBoxToList(pos, entityBox, collidingBoxes, BOT_AABB);
         }
     }
@@ -128,7 +144,8 @@ public class BlockRoof extends BlockBase {
     @Override
     public AxisAlignedBB getBoundingBox(IBlockState state, IBlockAccess source, BlockPos pos) {
         IBlockState actualState = state.getActualState(source, pos);
-        if (isConnected(actualState, EnumFacing.DOWN) || isConnected(actualState, EnumFacing.EAST) || isConnected(actualState, EnumFacing.WEST)) {
+        if (actualState.getValue(DOWN) || actualState.getValue(EAST) || actualState.getValue(WEST))
+        {
             return FULL_AABB;
         } else {
             return BASE_AABB;
