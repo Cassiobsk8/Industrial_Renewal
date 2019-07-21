fe75f7f86383d324c6841495ec7264cdd08fc7c2
CassioKF <cassiobsk8@hotmail.com>
2019-07-21 18:43:20 -0300

    Battery Output

--- src/main/java/cassiokf/industrialrenewal/blocks/BlockBatteryBank.java
+++ src/main/java/cassiokf/industrialrenewal/blocks/BlockBatteryBank.java
@@ -1,5 +1,6 @@
 package cassiokf.industrialrenewal.blocks;
 
+import cassiokf.industrialrenewal.init.ModItems;
 import cassiokf.industrialrenewal.tileentity.TileEntityBatteryBank;
 import net.minecraft.block.BlockHorizontal;
 import net.minecraft.block.material.Material;
@@ -10,12 +11,12 @@ import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.EntityLivingBase;
-import net.minecraft.tileentity.TileEntity;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumHand;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
-import net.minecraftforge.energy.CapabilityEnergy;
 
 import javax.annotation.Nullable;
 
@@ -26,35 +27,43 @@ public class BlockBatteryBank extends BlockTileEntity<TileEntityBatteryBank> {
     public static final PropertyBool NORTH = PropertyBool.create("north");
     public static final PropertyBool EAST = PropertyBool.create("east");
     public static final PropertyBool WEST = PropertyBool.create("west");
+    public static final PropertyBool UP = PropertyBool.create("up");
+    public static final PropertyBool DOWN = PropertyBool.create("down");
 
     public BlockBatteryBank(String name, CreativeTabs tab) {
         super(Material.IRON, name, tab);
+        setDefaultState(getDefaultState().withProperty(NORTH, false).withProperty(SOUTH, false)
+                .withProperty(EAST, false).withProperty(WEST, false)
+                .withProperty(UP, false).withProperty(DOWN, false));
     }
 
-    private boolean canConnectTo(IBlockAccess world, BlockPos pos, EnumFacing correspondentFacing)
-    {
-        if (world.getBlockState(pos).getValue(FACING).getOpposite() == correspondentFacing) return false;
-        TileEntity te = world.getTileEntity(pos.offset(correspondentFacing));
-        return te != null && te.hasCapability(CapabilityEnergy.ENERGY, correspondentFacing.getOpposite());
-    }
-
-    @SuppressWarnings("deprecation")
     @Override
-    public IBlockState getActualState(IBlockState state, final IBlockAccess world, final BlockPos pos) {
-        EnumFacing facing = state.getValue(FACING);
-        return state.withProperty(SOUTH, canConnectTo(world, pos, facing.getOpposite())).withProperty(NORTH, canConnectTo(world, pos, facing))
-                .withProperty(EAST, canConnectTo(world, pos, facing.rotateY())).withProperty(WEST, canConnectTo(world, pos, facing.rotateYCCW()));
+    public boolean onBlockActivated(World worldIn, BlockPos pos, IBlockState state, EntityPlayer playerIn, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ)
+    {
+        if (!worldIn.isRemote && hand == EnumHand.MAIN_HAND && playerIn.getHeldItem(EnumHand.MAIN_HAND).getItem().equals(ModItems.screwDrive) && worldIn.getTileEntity(pos) instanceof TileEntityBatteryBank)
+        {
+            TileEntityBatteryBank te = (TileEntityBatteryBank) worldIn.getTileEntity(pos);
+            te.toggleFacing(facing);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean rotateBlock(World world, BlockPos pos, EnumFacing axis)
+    {
+        return false;
     }
 
     @SuppressWarnings("deprecation")
     @Override
     public IBlockState getStateFromMeta(int meta) {
-        return getDefaultState().withProperty(FACING, EnumFacing.getHorizontal(meta));
+        return getDefaultState().withProperty(FACING, EnumFacing.byHorizontalIndex(meta));
     }
 
     @Override
     public int getMetaFromState(IBlockState state) {
-        return state.getValue(FACING).getIndex();
+        return state.getValue(FACING).getHorizontalIndex();
     }
 
     @SuppressWarnings("deprecation")
@@ -65,7 +74,30 @@ public class BlockBatteryBank extends BlockTileEntity<TileEntityBatteryBank> {
 
     @Override
     protected BlockStateContainer createBlockState() {
-        return new BlockStateContainer(this, FACING, SOUTH, NORTH, WEST, EAST);
+        return new BlockStateContainer(this, FACING, SOUTH, NORTH, WEST, EAST, UP, DOWN);
+    }
+
+    /*@SuppressWarnings("deprecation")
+    @Override
+    public IBlockState getActualState(IBlockState state, final IBlockAccess world, final BlockPos pos) {
+        EnumFacing facing = state.getValue(FACING);
+        return state.withProperty(SOUTH, canConnectTo(world, pos, facing.getOpposite()))
+                .withProperty(NORTH, canConnectTo(world, pos, facing))
+                .withProperty(EAST, canConnectTo(world, pos, facing.rotateY()))
+                .withProperty(WEST, canConnectTo(world, pos, facing.rotateYCCW()))
+                .withProperty(UP, canConnectTo(world, pos, EnumFacing.UP))
+                .withProperty(DOWN, canConnectTo(world, pos, EnumFacing.DOWN));
+    }*/
+
+    private boolean canConnectTo(IBlockAccess world, BlockPos pos, EnumFacing correspondentFacing)
+    {
+        if (world.getBlockState(pos).getValue(FACING).getOpposite() == correspondentFacing) return false;
+        TileEntityBatteryBank te = (TileEntityBatteryBank) world.getTileEntity(pos);
+        if (te != null)
+        {
+            return te.isFacingOutput(correspondentFacing);
+        }
+        return false;
     }
 
     @Override
--- src/main/java/cassiokf/industrialrenewal/tileentity/TileEntityBatteryBank.java
+++ src/main/java/cassiokf/industrialrenewal/tileentity/TileEntityBatteryBank.java
@@ -3,6 +3,7 @@ package cassiokf.industrialrenewal.tileentity;
 import cassiokf.industrialrenewal.blocks.BlockBatteryBank;
 import cassiokf.industrialrenewal.config.IRConfig;
 import cassiokf.industrialrenewal.util.VoltsEnergyContainer;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.EnumFacing;
@@ -15,10 +16,15 @@ import net.minecraftforge.energy.CapabilityEnergy;
 import net.minecraftforge.energy.IEnergyStorage;
 
 import javax.annotation.Nullable;
+import java.util.HashSet;
+import java.util.Set;
 
 public class TileEntityBatteryBank extends TileEntitySyncable implements ICapabilityProvider, ITickable
 {
     private final VoltsEnergyContainer container;
+    private final VoltsEnergyContainer dummyEnergy;
+    private final Set<EnumFacing> outPutFacings = new HashSet<>();
+    private boolean needSync = true;
 
     public TileEntityBatteryBank() {
         this.container = new VoltsEnergyContainer(IRConfig.MainConfig.Main.batteryBankCapacity, IRConfig.MainConfig.Main.batteryBankMaxInput, IRConfig.MainConfig.Main.batteryBankMaxOutput)
@@ -32,14 +38,27 @@ public class TileEntityBatteryBank extends TileEntitySyncable implements ICapabi
                 }
             }
         };
+        this.dummyEnergy = new VoltsEnergyContainer(0, 0, 0)
+        {
+            @Override
+            public boolean canReceive()
+            {
+                return false;
+            }
+        };
     }
 
     @Override
     public void update() {
         if (this.hasWorld() && !this.world.isRemote) {
+            if (needSync)
+            {
+                setState();
+            }
             World world = this.getWorld();
             BlockPos pos = this.getPos();
-            for (EnumFacing face : EnumFacing.VALUES) {
+            for (EnumFacing face : outPutFacings)
+            {
                 TileEntity te = world.getTileEntity(pos.offset(face));
                 if (te != null && te.hasCapability(CapabilityEnergy.ENERGY, face.getOpposite())
                         && (!(world.getBlockState(pos.offset(face)).getBlock() instanceof BlockBatteryBank) || (face == EnumFacing.DOWN && world.getBlockState(pos.offset(face)).getBlock() instanceof BlockBatteryBank))) {
@@ -50,6 +69,43 @@ public class TileEntityBatteryBank extends TileEntitySyncable implements ICapabi
                 }
             }
         }
+    }
+
+    public boolean toggleFacing(final EnumFacing facing)
+    {
+        if (outPutFacings.contains(facing))
+        {
+            outPutFacings.remove(facing);
+            setState();
+            return false;
+        } else
+        {
+            outPutFacings.add(facing);
+            setState();
+            return true;
+        }
+    }
+
+    private void setState()
+    {
+        if (!world.isRemote)
+        {
+            IBlockState state = world.getBlockState(pos);
+            EnumFacing facing = state.getValue(BlockBatteryBank.FACING);
+            state = state.withProperty(BlockBatteryBank.SOUTH, isFacingOutput(facing.getOpposite()))
+                    .withProperty(BlockBatteryBank.NORTH, isFacingOutput(facing))
+                    .withProperty(BlockBatteryBank.EAST, isFacingOutput(facing.rotateY()))
+                    .withProperty(BlockBatteryBank.WEST, isFacingOutput(facing.rotateYCCW()))
+                    .withProperty(BlockBatteryBank.UP, isFacingOutput(EnumFacing.UP))
+                    .withProperty(BlockBatteryBank.DOWN, isFacingOutput(EnumFacing.DOWN));
+            world.setBlockState(pos, state, 3);
+            this.Sync();
+        }
+    }
+
+    public boolean isFacingOutput(final @Nullable EnumFacing facing)
+    {
+        return outPutFacings.contains(facing) || facing == null;
     }
 
     public String GetText() {
@@ -77,12 +133,15 @@ public class TileEntityBatteryBank extends TileEntitySyncable implements ICapabi
 
     @Override
     public boolean hasCapability(Capability<?> capability, EnumFacing facing) {
+        if (capability == CapabilityEnergy.ENERGY && isFacingOutput(facing)) return true;
         return (capability == CapabilityEnergy.ENERGY && facing != getBlockFacing().getOpposite()) || super.hasCapability(capability, facing);
     }
 
     @Override
     @Nullable
     public <T> T getCapability(Capability<T> capability, @Nullable EnumFacing facing) {
+        if (capability == CapabilityEnergy.ENERGY && isFacingOutput(facing))
+            return CapabilityEnergy.ENERGY.cast(dummyEnergy);
         if (capability == CapabilityEnergy.ENERGY && facing != getBlockFacing().getOpposite())
             return CapabilityEnergy.ENERGY.cast(this.container);
         return super.getCapability(capability, facing);
@@ -90,13 +149,24 @@ public class TileEntityBatteryBank extends TileEntitySyncable implements ICapabi
 
     @Override
     public void readFromNBT(NBTTagCompound compound) {
-        super.readFromNBT(compound);
         this.container.deserializeNBT(compound.getCompoundTag("StoredIR"));
+
+        outPutFacings.clear();
+        final int[] enabledFacingIndices = compound.getIntArray("OutputFacings");
+        for (final int index : enabledFacingIndices)
+        {
+            outPutFacings.add(EnumFacing.byIndex(index));
+        }
+        super.readFromNBT(compound);
     }
 
     @Override
     public NBTTagCompound writeToNBT(NBTTagCompound compound) {
         compound.setTag("StoredIR", this.container.serializeNBT());
+        final int[] enabledFacingIndices = outPutFacings.stream()
+                .mapToInt(EnumFacing::getIndex)
+                .toArray();
+        compound.setIntArray("OutputFacings", enabledFacingIndices);
         return super.writeToNBT(compound);
     }
 }
