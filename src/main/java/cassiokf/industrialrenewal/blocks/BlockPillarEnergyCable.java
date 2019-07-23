8d3187b0cfdc60232f2349c61604181e7dc51784
CassioKF <cassiobsk8@hotmail.com>
2019-07-22 21:54:51 -0300

    Play sound on place

--- src/main/java/cassiokf/industrialrenewal/blocks/BlockPillarEnergyCable.java
+++ src/main/java/cassiokf/industrialrenewal/blocks/BlockPillarEnergyCable.java
@@ -14,9 +14,7 @@ import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.EnumBlockRenderType;
-import net.minecraft.util.EnumFacing;
-import net.minecraft.util.EnumHand;
+import net.minecraft.util.*;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
@@ -139,6 +137,7 @@ public class BlockPillarEnergyCable extends BlockEnergyCable
                 if (!world.isRemote)
                 {
                     world.setBlockState(pos.up(n), getBlockFromItem(playerItem).getDefaultState(), 3);
+                    world.playSound(null, pos, SoundEvent.REGISTRY.getObject(new ResourceLocation(("block.metal.place"))), SoundCategory.BLOCKS, 1f, 1f);
                     if (!player.isCreative()) playerStack.shrink(1);
                 }
                 return true;
