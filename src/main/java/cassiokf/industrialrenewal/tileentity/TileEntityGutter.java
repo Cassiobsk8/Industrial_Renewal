693db9c94835c30b9512aaba6af7bcf585d4ed19
CassioKF <cassiobsk8@hotmail.com>
2019-07-27 15:07:19 -0300

    fix Gutter

--- src/main/java/cassiokf/industrialrenewal/tileentity/TileEntityGutter.java
+++ src/main/java/cassiokf/industrialrenewal/tileentity/TileEntityGutter.java
@@ -2,240 +2,150 @@ package cassiokf.industrialrenewal.tileentity;
 
 import cassiokf.industrialrenewal.blocks.BlockGutter;
 import cassiokf.industrialrenewal.blocks.BlockRoof;
-import cassiokf.industrialrenewal.util.FluidTankUtils;
+import cassiokf.industrialrenewal.tileentity.tubes.TileEntityMultiBlocksTube;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.nbt.NBTTagCompound;
-import net.minecraft.network.NetworkManager;
-import net.minecraft.network.play.server.SPacketUpdateTileEntity;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.ITickable;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.World;
 import net.minecraftforge.common.capabilities.Capability;
+import net.minecraftforge.common.capabilities.ICapabilityProvider;
 import net.minecraftforge.fluids.FluidRegistry;
 import net.minecraftforge.fluids.FluidStack;
-import net.minecraftforge.fluids.capability.*;
+import net.minecraftforge.fluids.FluidTank;
+import net.minecraftforge.fluids.capability.CapabilityFluidHandler;
+import net.minecraftforge.fluids.capability.IFluidHandler;
 
 import javax.annotation.Nullable;
 
-public class TileEntityGutter extends TileFluidHandler implements IFluidHandler, ITickable
+public class TileEntityGutter extends TileEntityMultiBlocksTube<TileEntityGutter> implements ICapabilityProvider, ITickable
 {
 
-    private final long PERIOD = 400L;
-    private long lastTime = System.currentTimeMillis() - PERIOD;
-
-    public TileEntityGutter()
+    public FluidTank tank = new FluidTank(1000)
     {
-        tank = new FluidTankUtils(this, 1000);
-    }
-
-    @Override
-    public boolean shouldRefresh(World world, BlockPos pos, IBlockState oldState, IBlockState newState)
-    {
-        return (oldState.getBlock() != newState.getBlock());
-    }
+        @Override
+        public boolean canFill()
+        {
+            return false;
+        }
+    };
+    private int tick;
+    private int fillAmount;
 
     @Override
     public void update()
     {
-        if (this.hasWorld())
+        super.update();
+        if (this.hasWorld() && !world.isRemote)
         {
-            long thisTime = System.currentTimeMillis();
-            if (this.getWorld().isRaining())
+            if (isMaster())
             {
-                if ((thisTime - lastTime) >= PERIOD)
+                if (world.isRaining())
                 {
-                    lastTime = thisTime;
-                    World world = this.getWorld();
-                    IBlockState state = world.getBlockState(this.getPos());
-                    BlockPos pos = this.getPos();
-                    IBlockState actualstate = state.getActualState(world, pos);
-                    EnumFacing facing = EnumFacing.DOWN;
-                    Integer l = 1;
-                    Integer r = 1;
-                    Integer f = 1;
-                    Integer fillamount = 0;
-
-                    while (world.getBlockState(pos.offset(state.getValue(BlockGutter.FACING), f)).getBlock() instanceof BlockRoof)
+                    this.tank.fillInternal(new FluidStack(FluidRegistry.WATER, fillAmount), true);
+                }
+                if (this.tank.getFluidAmount() > 0)
+                {
+                    int quantity = getPosSet().size() > 0 ? (this.tank.getFluidAmount() / getPosSet().size()) : 0;
+                    for (BlockPos outPutPos : getPosSet().keySet())
                     {
-                        if (world.isRainingAt(pos.offset(state.getValue(BlockGutter.FACING), f).up(2)))
+                        final TileEntity tileEntity = world.getTileEntity(outPutPos);
+                        if (tileEntity != null && !tileEntity.isInvalid())
                         {
-                            fillamount++;
-                            f++;
-                        } else
-                        {
-                            f++;
-                        }
-                    }
-                    tank.fillInternal(new FluidStack(FluidRegistry.WATER, fillamount), true);
-
-                    if (actualstate.getValue(BlockGutter.ACTIVE_DOWN))
-                    {
-                        final TileEntity tileEntityD = world.getTileEntity(this.getPos().offset(EnumFacing.DOWN));
-                        if (tileEntityD != null && !tileEntityD.isInvalid())
-                        {
-                            if (tileEntityD.hasCapability(CapabilityFluidHandler.FLUID_HANDLER_CAPABILITY, EnumFacing.UP))
+                            EnumFacing facing = getPosSet().get(outPutPos);
+                            if (tileEntity.hasCapability(CapabilityFluidHandler.FLUID_HANDLER_CAPABILITY, facing.getOpposite()))
                             {
-                                IFluidHandler consumer = tileEntityD.getCapability(CapabilityFluidHandler.FLUID_HANDLER_CAPABILITY, EnumFacing.UP);
+                                final IFluidHandler consumer = tileEntity.getCapability(CapabilityFluidHandler.FLUID_HANDLER_CAPABILITY, facing.getOpposite());
                                 if (consumer != null)
                                 {
-                                    tank.drain(consumer.fill(tank.drain(tank.getCapacity(), false), true), true);
-                                }
-                            }
-                        }
-                    } else
-                    {
-                        while (world.getBlockState(pos.offset(state.getValue(BlockGutter.FACING).rotateY(), r)).getBlock() instanceof BlockGutter)
-                        {
-                            if (world.getBlockState(pos.offset(state.getValue(BlockGutter.FACING).rotateY(), r)).getActualState(world, pos.offset(state.getValue(BlockGutter.FACING).rotateY(), r)).getValue(BlockGutter.ACTIVE_DOWN))
-                            {
-                                r++;
-                                break;
-                            }
-                            if (world.getBlockState(pos.offset(state.getValue(BlockGutter.FACING).rotateY(), r)).getActualState(world, pos.offset(state.getValue(BlockGutter.FACING).rotateY(), r)).getValue(BlockGutter.ACTIVE_RIGHT))
-                            {
-                                r = r + 100;
-                                break;
-                            }
-                            r++;
-                        }
-                        while (world.getBlockState(pos.offset(state.getValue(BlockGutter.FACING).rotateY().getOpposite(), l)).getBlock() instanceof BlockGutter)
-                        {
-                            if (world.getBlockState(pos.offset(state.getValue(BlockGutter.FACING).rotateY().getOpposite(), l)).getActualState(world, pos.offset(state.getValue(BlockGutter.FACING).rotateY().getOpposite(), l)).getValue(BlockGutter.ACTIVE_DOWN))
-                            {
-                                l++;
-                                break;
-                            }
-                            if (world.getBlockState(pos.offset(state.getValue(BlockGutter.FACING).rotateY().getOpposite(), l)).getActualState(world, pos.offset(state.getValue(BlockGutter.FACING).rotateY().getOpposite(), l)).getValue(BlockGutter.ACTIVE_LEFT))
-                            {
-                                l = l + 100;
-                                break;
-                            }
-                            l++;
-                        }
-                        if (r < l)
-                        {
-                            facing = state.getValue(BlockGutter.FACING).rotateY();
-                        }
-                        if (r >= l)
-                        {
-                            facing = state.getValue(BlockGutter.FACING).rotateY().getOpposite();
-                        }
-                        final TileEntity tileEntityToFill = world.getTileEntity(pos.offset(facing));
-                        if (tileEntityToFill != null && !tileEntityToFill.isInvalid())
-                        {
-                            if (tileEntityToFill.hasCapability(CapabilityFluidHandler.FLUID_HANDLER_CAPABILITY, facing.getOpposite()))
-                            {
-                                IFluidHandler consumer = tileEntityToFill.getCapability(CapabilityFluidHandler.FLUID_HANDLER_CAPABILITY, facing.getOpposite());
-                                if (consumer != null)
-                                {
-                                    tank.drain(consumer.fill(tank.drain(tank.getCapacity(), false), true), true);
+                                    this.tank.drain(consumer.fill(this.tank.drain(quantity, false), true), true);
                                 }
                             }
                         }
                     }
                 }
             }
+            if (tick >= 100)
+            {
+                tick = 0;
+                if (world.isRaining()) checkIfIsReady();
+            }
+            tick++;
         }
     }
 
     @Override
-    public NBTTagCompound getUpdateTag()
+    public boolean instanceOf(TileEntity te)
     {
-        return writeToNBT(new NBTTagCompound());
-    }
-
-    @Nullable
-    @Override
-    public SPacketUpdateTileEntity getUpdatePacket()
-    {
-        return new SPacketUpdateTileEntity(getPos(), 0, getUpdateTag());
+        return te instanceof TileEntityGutter;
     }
 
     @Override
-    public void onDataPacket(final NetworkManager net, final SPacketUpdateTileEntity pkt)
+    public void checkForOutPuts(BlockPos bPos)
     {
-        readFromNBT(pkt.getNbtCompound());
-        notifyBlockUpdate();
+        if (world.isRemote) return;
+        EnumFacing face = EnumFacing.DOWN;
+        BlockPos currentPos = pos.offset(face);
+        IBlockState state = world.getBlockState(currentPos);
+        TileEntity te = world.getTileEntity(currentPos);
+        boolean hasMachine = !(state.getBlock() instanceof BlockGutter) && te != null && te.hasCapability(CapabilityFluidHandler.FLUID_HANDLER_CAPABILITY, face.getOpposite());
+        if (hasMachine && te.getCapability(CapabilityFluidHandler.FLUID_HANDLER_CAPABILITY, face.getOpposite()).getTankProperties()[0].canFill())
+            getMaster().addMachine(currentPos, face);
+        else getMaster().removeMachine(pos, currentPos);
     }
 
-    private void notifyBlockUpdate()
+    public void checkIfIsReady()
     {
-        final IBlockState state = getWorld().getBlockState(getPos());
-        getWorld().notifyBlockUpdate(getPos(), state, state, 3);
+        int f = 1;
+        fillAmount = 0;
+
+        EnumFacing face = getBlockFacing();
+        while (world.getBlockState(pos.offset(face, f)).getBlock() instanceof BlockRoof)
+        {
+            getMaster().fillAmount++;
+            f++;
+        }
     }
 
     @Override
-    public IFluidTankProperties[] getTankProperties()
+    public EnumFacing[] getFacesToCheck()
     {
-        return new IFluidTankProperties[]{new FluidTankProperties(tank.getInfo().fluid, tank.getInfo().capacity)};
+        EnumFacing facing = getBlockFacing();
+        return new EnumFacing[]{facing.rotateY(), facing.rotateYCCW()};
+    }
+
+    public EnumFacing getBlockFacing()
+    {
+        return world.getBlockState(pos).getValue(BlockGutter.FACING);
     }
 
     @Override
     public boolean hasCapability(final Capability<?> capability, @Nullable final EnumFacing facing)
     {
-        if (capability == CapabilityFluidHandler.FLUID_HANDLER_CAPABILITY)
-        {
-            return facing == EnumFacing.DOWN
-                    || facing == this.getWorld().getBlockState(this.getPos()).getValue(BlockGutter.FACING).rotateY()
-                    || facing == this.getWorld().getBlockState(this.getPos()).getValue(BlockGutter.FACING).rotateY().getOpposite();
-        }
-
-        return super.hasCapability(capability, facing);
+        return capability == CapabilityFluidHandler.FLUID_HANDLER_CAPABILITY && facing == EnumFacing.DOWN;
     }
 
     @Nullable
     @Override
     public <T> T getCapability(final Capability<T> capability, @Nullable final EnumFacing facing)
     {
-        if (capability == CapabilityFluidHandler.FLUID_HANDLER_CAPABILITY)
-        {
-            if (facing == this.getWorld().getBlockState(this.getPos()).getValue(BlockGutter.FACING).rotateY()
-                    || facing == this.getWorld().getBlockState(this.getPos()).getValue(BlockGutter.FACING).rotateY().getOpposite()
-                    || facing == EnumFacing.DOWN)
-            {
-                return CapabilityFluidHandler.FLUID_HANDLER_CAPABILITY.cast(tank);
-            }
-            return super.getCapability(capability, facing);
-        }
+        if (capability == CapabilityFluidHandler.FLUID_HANDLER_CAPABILITY && facing == EnumFacing.DOWN)
+            return CapabilityFluidHandler.FLUID_HANDLER_CAPABILITY.cast(getMaster().tank);
         return super.getCapability(capability, facing);
     }
 
     @Override
     public void readFromNBT(final NBTTagCompound tag)
     {
+        this.tank.readFromNBT(tag.getCompoundTag("tank"));
         super.readFromNBT(tag);
     }
 
     @Override
     public NBTTagCompound writeToNBT(final NBTTagCompound tag)
     {
+        tag.setTag("tank", this.tank.writeToNBT(new NBTTagCompound()));
         return super.writeToNBT(tag);
-    }
-
-    @Override
-    public int fill(FluidStack resource, boolean doFill)
-    {
-        if (resource == null)
-            return 0;
-        int canAccept = resource.amount;
-        if (canAccept <= 0)
-            return 0;
-
-        return this.tank.fill(resource, doFill);
-    }
-
-    @Override
-    public FluidStack drain(FluidStack resource, boolean doDrain)
-    {
-        return this.tank.drain(resource.amount, doDrain);
-    }
-
-    @Override
-    public FluidStack drain(int maxDrain, boolean doDrain)
-    {
-        //needsUpdate = true;
-        return this.tank.drain(maxDrain, doDrain);
     }
 }
