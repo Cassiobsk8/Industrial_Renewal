c3cca66c3c848ff625a71b3dc83cc007030c0d51
CassioKF <cassiobsk8@hotmail.com>
2020-10-26 17:15:30 -0300

    Optimize carts items

--- src/main/java/cassiokf/industrialrenewal/init/ModItems.java
+++ src/main/java/cassiokf/industrialrenewal/init/ModItems.java
@@ -4,6 +4,7 @@ import cassiokf.industrialrenewal.References;
 import cassiokf.industrialrenewal.item.*;
 import cassiokf.industrialrenewal.item.armor.ItemSafetyBelt;
 import cassiokf.industrialrenewal.item.armor.ItemSafetyHelmet;
+import cassiokf.industrialrenewal.item.carts.*;
 import net.minecraft.item.Item;
 import net.minecraftforge.registries.IForgeRegistry;
 
--- src/main/java/cassiokf/industrialrenewal/item/carts/ItemCartBase.java
+++ src/main/java/cassiokf/industrialrenewal/item/carts/ItemCartBase.java
@@ -1,9 +1,10 @@
-package cassiokf.industrialrenewal.item;
+package cassiokf.industrialrenewal.item.carts;
 
-import cassiokf.industrialrenewal.entity.EntityPassengerCar;
+import cassiokf.industrialrenewal.item.ItemBase;
 import net.minecraft.block.BlockRailBase;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.item.EntityMinecart;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.EnumActionResult;
@@ -12,35 +13,41 @@ import net.minecraft.util.EnumHand;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
-public class ItemMineCartPassengerCar extends ItemBase {
-
-
-    public ItemMineCartPassengerCar(String name, CreativeTabs tab) {
+public abstract class ItemCartBase extends ItemBase
+{
+    public ItemCartBase(String name, CreativeTabs tab)
+    {
         super(name, tab);
 
         this.maxStackSize = 16;
     }
 
     @Override
-    public EnumActionResult onItemUse(EntityPlayer player, World worldIn, BlockPos pos, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ) {
+    public EnumActionResult onItemUse(EntityPlayer player, World worldIn, BlockPos pos, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ)
+    {
         IBlockState iblockstate = worldIn.getBlockState(pos);
 
-        if (!BlockRailBase.isRailBlock(iblockstate)) {
+        if (!BlockRailBase.isRailBlock(iblockstate))
+        {
             return EnumActionResult.FAIL;
-        } else {
+        } else
+        {
             ItemStack itemstack = player.getHeldItem(hand);
 
-            if (!worldIn.isRemote) {
+            if (!worldIn.isRemote)
+            {
                 BlockRailBase.EnumRailDirection blockrailbase$enumraildirection = iblockstate.getBlock() instanceof BlockRailBase ? ((BlockRailBase) iblockstate.getBlock()).getRailDirection(worldIn, pos, iblockstate, null) : BlockRailBase.EnumRailDirection.NORTH_SOUTH;
                 double d0 = 0.0D;
 
-                if (blockrailbase$enumraildirection.isAscending()) {
+                if (blockrailbase$enumraildirection.isAscending())
+                {
                     d0 = 0.5D;
                 }
 
-                EntityPassengerCar entityminecart = new EntityPassengerCar(worldIn, (double) pos.getX() + 0.5D, (double) pos.getY() + 0.0625D + d0, (double) pos.getZ() + 0.5D);
+                EntityMinecart entityminecart = getEntity(worldIn, (double) pos.getX() + 0.5D, (double) pos.getY() + 0.0625D + d0, (double) pos.getZ() + 0.5D);
 
-                if (itemstack.hasDisplayName()) {
+                if (itemstack.hasDisplayName())
+                {
                     entityminecart.setCustomNameTag(itemstack.getDisplayName());
                 }
 
@@ -52,4 +59,5 @@ public class ItemMineCartPassengerCar extends ItemBase {
         }
     }
 
+    public abstract EntityMinecart getEntity(World world, double x, double y, double z);
 }
