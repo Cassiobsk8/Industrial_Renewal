5181b9b948b06f24507856f843e76d752537d450
CassioKF <cassiobsk8@hotmail.com>
2020-11-10 23:25:40 -0300

    new Prospecting Pan

--- src/main/java/cassiokf/industrialrenewal/item/ItemProspectingPan.java
+++ src/main/java/cassiokf/industrialrenewal/item/ItemProspectingPan.java
@@ -5,14 +5,17 @@ import cassiokf.industrialrenewal.util.MachinesUtils;
 import cassiokf.industrialrenewal.util.Utils;
 import net.minecraft.client.resources.I18n;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.IItemPropertyGetter;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.ActionResult;
-import net.minecraft.util.EnumActionResult;
-import net.minecraft.util.EnumFacing;
-import net.minecraft.util.EnumHand;
+import net.minecraft.util.*;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+import javax.annotation.Nullable;
 
 public class ItemProspectingPan extends ItemBase
 {
@@ -22,14 +25,14 @@ public class ItemProspectingPan extends ItemBase
         this.maxStackSize = 1;
         this.setMaxDamage(17);
         setContainerItem(this);
-        //this.addPropertyOverride(new ResourceLocation("broken"), new IItemPropertyGetter()
-        //{
-        //    @SideOnly(Side.CLIENT)
-        //    public float apply(ItemStack stack, @Nullable World worldIn, @Nullable EntityLivingBase entityIn)
-        //    {
-        //        return ItemProspectingPan.isEmpty(stack) ? 0.0F : 1.0F;
-        //    }
-        //});
+        this.addPropertyOverride(new ResourceLocation("broken"), new IItemPropertyGetter()
+        {
+            @SideOnly(Side.CLIENT)
+            public float apply(ItemStack stack, @Nullable World worldIn, @Nullable EntityLivingBase entityIn)
+            {
+                return ItemProspectingPan.isEmpty(stack) ? 0.0F : 1.0F;
+            }
+        });
     }
 
     public static boolean isEmpty(ItemStack stack)
@@ -38,10 +41,16 @@ public class ItemProspectingPan extends ItemBase
     }
 
     @Override
+    public boolean getIsRepairable(ItemStack itemToRepair, ItemStack resourceItem)
+    {
+        return false;
+    }
+
+    @Override
     public EnumActionResult onItemUse(EntityPlayer player, World worldIn, BlockPos pos, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ)
     {
         ItemStack stack = player.getHeldItem(hand);
-        if (worldIn.provider.getDimension() == 0 && getDamage(stack) == 0)
+        if (worldIn.provider.getDimension() == 0 && stack.getItemDamage() <= 0)
         {
             if (!worldIn.isRemote) stack.setItemDamage(1);
             return EnumActionResult.SUCCESS;
@@ -56,7 +65,7 @@ public class ItemProspectingPan extends ItemBase
         if (worldIn.provider.getDimension() == 0 && stack.getItemDamage() > 0)
         {
             playerIn.swingArm(handIn);
-            if (!worldIn.isRemote) prospect(worldIn, playerIn, handIn, stack);
+            prospect(worldIn, playerIn, handIn, stack);
             return new ActionResult<>(EnumActionResult.PASS, playerIn.getHeldItem(handIn));
         }
         return super.onItemRightClick(worldIn, playerIn, handIn);
@@ -65,7 +74,7 @@ public class ItemProspectingPan extends ItemBase
     private void prospect(World worldIn, EntityPlayer playerIn, EnumHand hand, ItemStack stack)
     {
         stack.damageItem(1, playerIn);
-        if (stack.getItemDamage() >= 17)
+        if (stack.getItemDamage() >= 17 && !worldIn.isRemote)
         {
             TEDeepVein vein = MachinesUtils.getDeepVein(worldIn, playerIn.getPosition());
             ItemStack stackv = vein != null ? vein.getOre(0, true) : ItemStack.EMPTY;
