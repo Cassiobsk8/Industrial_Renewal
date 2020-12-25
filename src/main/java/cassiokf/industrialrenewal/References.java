c691b24ee1164aab782d11236ebe77f0b0bbc22b
CassioKF <cassiobsk8@hotmail.com>
2020-12-25 18:40:40 -0300

    0.21.3 Released

--- src/main/java/cassiokf/industrialrenewal/References.java
+++ src/main/java/cassiokf/industrialrenewal/References.java
@@ -10,7 +10,7 @@ public class References {
 
     public static final String NAME = "Industrial Renewal";
 
-    public static final String VERSION = "0.21.2";
+    public static final String VERSION = "0.21.3";
 
     public static final String NETWORKCHANNEL = MODID;
 
