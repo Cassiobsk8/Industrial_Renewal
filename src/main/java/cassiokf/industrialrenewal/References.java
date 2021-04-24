493a6cdb8cbc8ee69c5d0fd335c1a89ae430ab20
CassioKF <cassiobsk8@hotmail.com>
2021-04-24 16:29:01 -0300

    0.21.7 preparation

--- src/main/java/cassiokf/industrialrenewal/References.java
+++ src/main/java/cassiokf/industrialrenewal/References.java
@@ -10,7 +10,7 @@ public class References {
 
     public static final String NAME = "Industrial Renewal";
 
-    public static final String VERSION = "0.21.5";
+    public static final String VERSION = "0.21.7a";
 
     public static final String NETWORKCHANNEL = MODID;
 
