f3aa9fa727ac1c9bf7dc5e3bed33ade205083bc4
CassioKF <cassiobsk8@hotmail.com>
2025-03-17 23:21:14 -0300

    Refactor BlockEntityTransformer for improved energy distribution and percentage calculation

--- src/main/java/net/cassiokf/industrialrenewal/blockentity/BlockEntityTransformer.java
+++ src/main/java/net/cassiokf/industrialrenewal/blockentity/BlockEntityTransformer.java
@@ -4,6 +4,8 @@ import net.cassiokf.industrialrenewal.block.BlockTransformer;
 import net.cassiokf.industrialrenewal.blockentity.abstracts.BlockEntity3x2x3MachineBase;
 import net.cassiokf.industrialrenewal.init.ModBlockEntity;
 import net.cassiokf.industrialrenewal.init.ModBlocks;
+import net.cassiokf.industrialrenewal.obj.CapPercentage;
+import net.cassiokf.industrialrenewal.util.Utils;
 import net.cassiokf.industrialrenewal.util.capability.CustomEnergyStorage;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
@@ -17,20 +19,20 @@ import net.minecraftforge.energy.IEnergyStorage;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
+import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 import java.util.stream.Collectors;
 
 public class BlockEntityTransformer extends BlockEntity3x2x3MachineBase<BlockEntityTransformer> {
     
-    private final int MAX_CAPACITY = 10000;
-    private final int TRANSFER_SPEED = 10000;
+    private static int TRANSFER_SPEED = 10240;
     private final CustomEnergyStorage energyDummy = new CustomEnergyStorage(0);
     private final LazyOptional<CustomEnergyStorage> dummyHandler = LazyOptional.of(() -> energyDummy);
     private int tick = 0;
     private int averageEnergy = 0;
     private int energyOverTime = 0;
-    private final CustomEnergyStorage energyStorage = new CustomEnergyStorage(MAX_CAPACITY, TRANSFER_SPEED, TRANSFER_SPEED) {
+    private final CustomEnergyStorage energyStorage = new CustomEnergyStorage(TRANSFER_SPEED, TRANSFER_SPEED, TRANSFER_SPEED) {
         
         @Override
         public int receiveEnergy(int maxReceive, boolean simulate) {
@@ -78,20 +80,50 @@ public class BlockEntityTransformer extends BlockEntity3x2x3MachineBase<BlockEnt
             Set<BlockPos> allNodesPos = isolator.allNodes;
             Set<BlockEntityTransformer> availableTransformers = allNodesPos.stream().filter(x -> level.getBlockEntity(x.below()) instanceof BlockEntityTransformer).map(x -> (BlockEntityTransformer) level.getBlockEntity(x.below())).filter(Objects::nonNull).filter(x -> x.isMaster() && x.extract()).collect(Collectors.toSet());
             if (availableTransformers.isEmpty()) return 0;
-            int quota = Math.min(TRANSFER_SPEED, energy) / availableTransformers.size();
+            int realMaxEnergy = Math.min(TRANSFER_SPEED, energy);
             
-            for (BlockEntityTransformer transformer : availableTransformers) {
-                if (transformer != null) {
-                    int amount = transformer.outPutEnergy(quota, simulate);
-                    //Utils.debug("REQUEST", worldPosition, transformer, quota, amount, this.energyStorage.getMaxEnergyStored()-this.energyStorage.getEnergyStored());
-                    //quota += (quota - amount);
-                    out += amount;
+            Set<CapPercentage> percentages = calculatePercentage(availableTransformers, realMaxEnergy);
+            
+            for (CapPercentage entry : percentages) {
+                float percentage = entry.getPercentage();
+                BlockEntityTransformer transformer = (BlockEntityTransformer) entry.getBlockEntity();
+                int individualAmount = entry.getAmount();
+                int energyCalculated = (int) (individualAmount * percentage);
+                if (energyCalculated > 0) {
+                    int energyOut = transformer.outPutEnergy(energyCalculated, simulate);
+                    out += energyOut;
                 }
-                if (quota <= 0) break;
             }
         }
         if (!simulate) energyOverTime += out;
         return out;
+    }
+    
+    private Set<CapPercentage> calculatePercentage(Set<BlockEntityTransformer> availableTransformers, int energy) {
+        int totalEnergyUsed = 0;
+        int validOutputs = 0;
+        Set<CapPercentage> percentages = new HashSet<>();
+        for (BlockEntityTransformer transformer : availableTransformers) {
+            if (transformer != null) {
+                int amount = transformer.outPutEnergy(energy, true);
+                if (amount > 0) {
+                    totalEnergyUsed += amount;
+                    validOutputs++;
+                    percentages.add(new CapPercentage((IEnergyStorage) null, transformer, 1f, amount));
+                }
+            }
+        }
+        
+        if (totalEnergyUsed <= energy || validOutputs == 0 || totalEnergyUsed == 0) return percentages;
+        
+        float bestRatio = Utils.normalize(energy, 0, totalEnergyUsed);
+        for (CapPercentage entry : percentages) {
+            float actualPercentage = entry.getPercentage();
+            if (actualPercentage > 0f) {
+                entry.setPercentage(actualPercentage * bestRatio);
+            }
+        }
+        return percentages;
     }
     
     private int outPutEnergy(int energy, boolean simulate) {
@@ -126,7 +158,7 @@ public class BlockEntityTransformer extends BlockEntity3x2x3MachineBase<BlockEnt
     
     public float getGenerationFill() {
         float currentAmount = averageEnergy;
-        float totalCapacity = MAX_CAPACITY;
+        float totalCapacity = TRANSFER_SPEED;
         //        Utils.debug("G", currentAmount, totalCapacity);
         currentAmount = currentAmount / totalCapacity;
         return currentAmount * 90f;
